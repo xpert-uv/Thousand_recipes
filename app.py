3229386e402866190ad72d47fa3be357b9ee0761
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2021-03-15 22:41:22 -0400

    resualbe-functions

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, g, jsonify
 from model import db, connect_db, User, Food, Cocktails, Wine, Wineparing
+from myfunc import Search_recipe, Recipe_details, Search_wine, Wine_paring_for_meal, Dish_paring_for_wine
 import keys
 import os
 import requests
 
+CURR_USER_KEY = keys.CURR_USER_KEY
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -13,3 +15,43 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get(keys.SECRET_KEY, 'SQLALCHEMY')
 
 connect_db(app)
+
+""" api setups:
+using keywords: https://api.spoonacular.com/recipes/search?query=pasta&apiKey={keys.s_api}
+
+recipe's details: https://api.spoonacular.com/recipes/{id}/information?apiKey={keys.s_api}
+get ingdrents by id: https://api.spoonacular.com/recipes/715538/ingredientWidget.json?apiKey=1edd0ae07bdd44df9d7fbde570f1134a
+
+wine:
+keywords:https://api.spoonacular.com/food/wine/recommendation?wine=merlot&number=2&apiKey=1edd0ae07bdd44df9d7fbde570f1134a
+Dish_Paring_For_Wine: https://api.spoonacular.com/food/wine/dishes?wine=malbec&apiKey={keys.s_api}
+Wine_Paring_For_Food: https://api.spoonacular.com/food/wine/pairing?food=steak&apiKey={keys.s_api}
+
+cocktails:
+
+
+"""
+
+
+def add_user_to_g():
+    """ If user is logged in, add user to FLASK GLOBAL."""
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    else:
+        g.user = None
+
+
+def do_login(user):
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/')
+def landingpage():
+    return render_template("home.html")
---
+++ myfunc.py
@@ -0,0 +1,127 @@
+
+import pdb
+from flask import jsonify
+import requests
+from keys import s_api
+
+BASE_API = "https://api.spoonacular.com"
+
+
+##################### Function for Food Recipes ###################################
+def Search_recipe(recipename):
+    """ This function will make request to api about lists of recipes based on keyword like: pasta, chicken, cake etc
+     and returns the filtered data in json format. """
+
+    response = requests.get(
+        f'{BASE_API}/recipes/search?query={recipename}&apiKey={s_api}')
+    json_response = response.json()
+    result_list = []
+    # for image url
+    imageurl = json_response['baseUri']
+
+    for r in json_response['results']:
+        response = {'id': r['id'],
+                    'title': r['title'],
+                    'total_time': r['readyInMinutes'],
+                    'image': imageurl + r['image']}
+        result_list.append(response)
+
+    final_response = {"result": result_list}
+    return final_response
+
+
+def Recipe_details(recipe_id):
+    """ This function will make request to API about the recpie details base on id and returns the filtered data in json format"""
+
+    response = requests.get(
+        f'{BASE_API}/recipes/{recipe_id}/information?apiKey={s_api}')
+    json_response = response.json()
+
+    # API request to get Ingredients-by-id
+    response_ing = requests.get(
+        f'{BASE_API}/recipes/{recipe_id}/ingredientWidget.json?apiKey={s_api}')
+    json_response_ing = response_ing.json()
+
+    # striping individual data of ingredients
+    ingredietns_list = []
+    ingredients_name = [x['name'] for x in json_response_ing['ingredients']]
+    ingredients_amount = [x['amount']for x in json_response_ing['ingredients']]
+    ingredients_value = [x['us']['value'] for x in ingredients_amount]
+    ingredietns_unit = [x['us']['unit'] for x in ingredients_amount]
+
+    # zipping name, value, unit data to form a sentence and creating list of ingredietns
+
+    for name, value, unit in zip(ingredients_name, ingredients_value, ingredietns_unit):
+        ingredietns_list.append(f'{name} {value} {unit}')
+
+    final_response = {'id': json_response['id'],
+                      'title': json_response['title'],
+                      'ingredients': ingredietns_list,
+                      'total_time': json_response['readyInMinutes'],
+                      'image': json_response['image'],
+                      'description': json_response['summary'],
+                      'instructions': json_response['instructions']}
+
+    return final_response
+
+
+############################# Functions for Wine and Wine paring #########################
+
+def Search_wine(wine_name):
+    """ This function will request the API about the Wine based on wine name/type  and returns the filtred data in json format.
+        -Default numbers of wine searched return is 20.
+
+
+    """
+    response = requests.get(
+        f'{BASE_API}/food/wine/recommendation?wine={wine_name}&number=20&apiKey={s_api}')
+    json_response = response.json()
+
+    wine_list = []
+    for wines in json_response['recommendedWines']:
+        wine = {'id': wines['id'],
+                'title': wines['title'],
+                'description': wines['description'],
+                'price': wines['price'],
+                'image': wines['imageUrl']
+                }
+        wine_list.append(wine)
+    final_response = {"wines": wine_list}
+    return final_response
+
+
+def Wine_paring_for_meal(food):
+    """ This function will make a request to API and pair wine for select meal/food and 
+    returns data into jason format. """
+
+    response = requests.get(
+        f'{BASE_API}/food/wine/pairing?food={food}&apiKey={s_api}')
+    json_response = response.json()
+
+    wine_list = []
+    for wines in json_response['productMatches']:
+
+        wine = {'title': wines['title'],
+                'description': wines['description'],
+                'price': wines['price'],
+                'image': wines['imageUrl']
+                }
+        wine_list.append(wine)
+
+    final_response = {'wines': json_response['pairedWines'],
+                      'description': json_response['pairingText'],
+                      'wine': wine_list}
+
+    return final_response
+
+
+def Dish_paring_for_wine(wine):
+    """ This function will make a request to API and pair dish for selected wine and returns data into jason format"""
+
+    response = requests.get(
+        f'{BASE_API}/food/wine/dishes?wine={wine}&apiKey={s_api}')
+    json_response = response.json()
+
+    final_response = {'paring': json_response['pairings']}
+
+    return final_response
