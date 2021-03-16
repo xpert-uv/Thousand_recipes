59fc0a9e68baaed5d64e468e3a8f1b01c21b0ab6
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2021-03-15 23:28:52 -0400

    Error handling Updated for function

--- myfunc.py
+++ myfunc.py
@@ -15,19 +15,22 @@ def Search_recipe(recipename):
     response = requests.get(
         f'{BASE_API}/recipes/search?query={recipename}&apiKey={s_api}')
     json_response = response.json()
-    result_list = []
-    # for image url
-    imageurl = json_response['baseUri']
+    if response.status_code != 200:
+        return json_response
+    else:
+        result_list = []
+        # for image url
+        imageurl = json_response['baseUri']
 
-    for r in json_response['results']:
-        response = {'id': r['id'],
-                    'title': r['title'],
-                    'total_time': r['readyInMinutes'],
-                    'image': imageurl + r['image']}
-        result_list.append(response)
+        for r in json_response['results']:
+            response = {'id': r['id'],
+                        'title': r['title'],
+                        'total_time': r['readyInMinutes'],
+                        'image': imageurl + r['image']}
+            result_list.append(response)
 
-    final_response = {"result": result_list}
-    return final_response
+        final_response = {"result": result_list}
+        return final_response
 
 
 def Recipe_details(recipe_id):
@@ -42,27 +45,34 @@ def Recipe_details(recipe_id):
         f'{BASE_API}/recipes/{recipe_id}/ingredientWidget.json?apiKey={s_api}')
     json_response_ing = response_ing.json()
 
-    # striping individual data of ingredients
-    ingredietns_list = []
-    ingredients_name = [x['name'] for x in json_response_ing['ingredients']]
-    ingredients_amount = [x['amount']for x in json_response_ing['ingredients']]
-    ingredients_value = [x['us']['value'] for x in ingredients_amount]
-    ingredietns_unit = [x['us']['unit'] for x in ingredients_amount]
+    if response.status_code != 200:
+        return json_response
+    elif response_ing.status_code != 200:
+        return json_response_ing
+    else:
+        # striping individual data of ingredients
+        ingredietns_list = []
+        ingredients_name = [x['name']
+                            for x in json_response_ing['ingredients']]
+        ingredients_amount = [x['amount']
+                              for x in json_response_ing['ingredients']]
+        ingredients_value = [x['us']['value'] for x in ingredients_amount]
+        ingredietns_unit = [x['us']['unit'] for x in ingredients_amount]
 
-    # zipping name, value, unit data to form a sentence and creating list of ingredietns
+        # zipping name, value, unit data to form a sentence and creating list of ingredietns
 
-    for name, value, unit in zip(ingredients_name, ingredients_value, ingredietns_unit):
-        ingredietns_list.append(f'{name} {value} {unit}')
+        for name, value, unit in zip(ingredients_name, ingredients_value, ingredietns_unit):
+            ingredietns_list.append(f'{name} {value} {unit}')
 
-    final_response = {'id': json_response['id'],
-                      'title': json_response['title'],
-                      'ingredients': ingredietns_list,
-                      'total_time': json_response['readyInMinutes'],
-                      'image': json_response['image'],
-                      'description': json_response['summary'],
-                      'instructions': json_response['instructions']}
+        final_response = {'id': json_response['id'],
+                          'title': json_response['title'],
+                          'ingredients': ingredietns_list,
+                          'total_time': json_response['readyInMinutes'],
+                          'image': json_response['image'],
+                          'description': json_response['summary'],
+                          'instructions': json_response['instructions']}
 
-    return final_response
+        return final_response
 
 
 ############################# Functions for Wine and Wine paring #########################
@@ -76,43 +86,49 @@ def Search_wine(wine_name):
     response = requests.get(
         f'{BASE_API}/food/wine/recommendation?wine={wine_name}&number=20&apiKey={s_api}')
     json_response = response.json()
+    if response.status_code != 200:
+        return json_response
+    else:
+        wine_list = []
+        for wines in json_response['recommendedWines']:
+            wine = {'id': wines['id'],
+                    'title': wines['title'],
+                    'description': wines['description'],
+                    'price': wines['price'],
+                    'image': wines['imageUrl']
+                    }
+            wine_list.append(wine)
 
-    wine_list = []
-    for wines in json_response['recommendedWines']:
-        wine = {'id': wines['id'],
-                'title': wines['title'],
-                'description': wines['description'],
-                'price': wines['price'],
-                'image': wines['imageUrl']
-                }
-        wine_list.append(wine)
-    final_response = {"wines": wine_list}
-    return final_response
+        final_response = {"wines": wine_list}
+        return final_response
 
 
 def Wine_paring_for_meal(food):
-    """ This function will make a request to API and pair wine for select meal/food and 
+    """ This function will make a request to API and pair wine for select meal/food and
     returns data into jason format. """
 
     response = requests.get(
         f'{BASE_API}/food/wine/pairing?food={food}&apiKey={s_api}')
     json_response = response.json()
+    if response.status_code != 200:
+        return json_response
+    else:
 
-    wine_list = []
-    for wines in json_response['productMatches']:
+        wine_list = []
+        for wines in json_response['productMatches']:
 
-        wine = {'title': wines['title'],
-                'description': wines['description'],
-                'price': wines['price'],
-                'image': wines['imageUrl']
-                }
-        wine_list.append(wine)
+            wine = {'title': wines['title'],
+                    'description': wines['description'],
+                    'price': wines['price'],
+                    'image': wines['imageUrl']
+                    }
+            wine_list.append(wine)
 
-    final_response = {'wines': json_response['pairedWines'],
-                      'description': json_response['pairingText'],
-                      'wine': wine_list}
+        final_response = {'wines': json_response['pairedWines'],
+                          'description': json_response['pairingText'],
+                          'wine': wine_list}
 
-    return final_response
+        return final_response
 
 
 def Dish_paring_for_wine(wine):
@@ -121,7 +137,9 @@ def Dish_paring_for_wine(wine):
     response = requests.get(
         f'{BASE_API}/food/wine/dishes?wine={wine}&apiKey={s_api}')
     json_response = response.json()
+    if response.status_code != 200:
+        return json_response
+    else:
+        final_response = {'paring': json_response['pairings']}
 
-    final_response = {'paring': json_response['pairings']}
-
-    return final_response
+        return final_response
