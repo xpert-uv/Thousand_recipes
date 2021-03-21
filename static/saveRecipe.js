45318440e2485d5ed83ac00e80f1f69a946d3c19
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2021-03-21 03:38:46 -0400

    'finial'

--- static/saveRecipe.js
+++ static/saveRecipe.js
@@ -8,15 +8,15 @@ $(".card-body").on("click", "#wine-btn", paringWine);
 function generateWineHTML_tabs(wine) {
 
     return ` <li class="nav-item" role="presentation">
-            <button class="nav-link" id="${wine}-tab" data-bs-toggle="tab" data-bs-target="#${wine}" type="button"
-                role="tab" aria-controls="${wine}" aria-selected="true" data-name="${wine}">${wine}</button>
+            <a class="nav-link " id="${wine}-tab" data-bs-toggle="tab" data-bs-target="#${wine}" type="button"
+                role="tab" aria-controls="${wine}" aria-selected="true" data-id="${wine}">${wine}</a>
         </li>`;
 
 }
 
 function generateWineHTML_tab_pannel(wine) {
     return `
-         <div class="tab-pane fade" id="${wine}" role="tabpanel" aria-labelledby="${wine}-tab">...</div>
+         <div class="tab-pane fade row d-flex" id="${wine}" role="tabpanel" aria-labelledby="${wine}-tab">...</div>
         `;
 }
 
@@ -46,6 +46,7 @@ async function paringWine() {
             let tab_pannels = generateWineHTML_tab_pannel(wines);
             $("#myTab").append(msg);
             $("#myTabContent").append(tab_pannels);
+            break;
         };
     }
 }
@@ -53,11 +54,12 @@ async function paringWine() {
 /////////////////////////////////////////////////////////////////////////////////////
 
 
-$("#myTab").on("click", ".nav-link active", searchWine);
+$("#myTab").on("click", ".nav-item", searchWine);
 
 async function searchWine() {
-    const wineType = $(this).data('name');
-    response = await axios.get(`${BASE_API}/searchwine/wineType`);
+    const wineType = $(this).children().data("id");
+    console.log(wineType);
+    const response = await axios.get(`${BASE_API}/searchwine/${wineType}`);
 
     for (let w_data of response.data.wines) {
         let wine = $(generateWineHTML(w_data));
@@ -68,15 +70,15 @@ async function searchWine() {
 
 function generateWineHTML(wine) {
     return `<div class="col-sm-4">
-             <div class="card mt-3" style:witdh=100px;>
-            <img src="${wine.image}" class="card-img-top" alt="...">
+             <div class="card mt-3">
+            <img src="${wine.image}" class="card-img-top" alt="..." style:witdh=100px, height=100px;>
             <div class="card-body text-dark">
             <ul>
             <li>${wine.title}</li>
              <li>${wine.price}</li>
             <li>Info:<br>${wine.description}</li>
             </ul>
-            <a id="view-btn" type="button" class="btn btn-danger view-btn" href='${BASE_API}/recipedetail/${wine.id}' data-id=${wine.id}>View</a>
+           
             </div>
             </div>
             </div>`;
